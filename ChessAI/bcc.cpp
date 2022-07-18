7508763ed65f49cf456895fb5dd74c19b5839348
Maser-GitHub <simone000161@gmail.com>
2022-07-18 13:16:25 +0200

    Add evaluation system

--- ChessAI/bcc.cpp
+++ ChessAI/bcc.cpp
@@ -167,7 +167,164 @@ static inline int get_ls1b_index(U64 bitboard) {
 }
 
 
+// material scrore
+
+
+int material_score[12] = {
+	100,      // white pawn score
+	300,      // white knight scrore
+	350,      // white bishop score
+	500,      // white rook score
+   1000,      // white queen score
+  10000,      // white king score
+   -100,      // black pawn score
+   -300,      // black knight scrore
+   -350,      // black bishop score
+   -500,      // black rook score
+  -1000,      // black queen score
+ -10000,      // black king score
+};
+
+
+//This will be the core of the AI
+
+// pawn positional score
+const int pawn_score[64] =
+{
+	90,  90,  90,  90,  90,  90,  90,  90,
+	30,  30,  30,  40,  40,  30,  30,  30,
+	20,  20,  20,  30,  30,  30,  20,  20,
+	10,  10,  10,  20,  20,  10,  10,  10,
+	 5,   5,  10,  20,  20,   5,   5,   5,
+	 0,   0,   0,   5,   5,   0,   0,   0,
+	 0,   0,   0, -10, -10,   0,   0,   0,
+	 0,   0,   0,   0,   0,   0,   0,   0
+};
+
+// knight positional score
+const int knight_score[64] =
+{
+	-5,   0,   0,   0,   0,   0,   0,  -5,
+	-5,   0,   0,  10,  10,   0,   0,  -5,
+	-5,   5,  20,  20,  20,  20,   5,  -5,
+	-5,  10,  20,  30,  30,  20,  10,  -5,
+	-5,  10,  20,  30,  30,  20,  10,  -5,
+	-5,   5,  20,  10,  10,  20,   5,  -5,
+	-5,   0,   0,   0,   0,   0,   0,  -5,
+	-5, -10,   0,   0,   0,   0, -10,  -5
+};
+
+// bishop positional score
+const int bishop_score[64] =
+{
+	 0,   0,   0,   0,   0,   0,   0,   0,
+	 0,   0,   0,   0,   0,   0,   0,   0,
+	 0,   0,   0,  10,  10,   0,   0,   0,
+	 0,   0,  10,  20,  20,  10,   0,   0,
+	 0,   0,  10,  20,  20,  10,   0,   0,
+	 0,  10,   0,   0,   0,   0,  10,   0,
+	 0,  30,   0,   0,   0,   0,  30,   0,
+	 0,   0, -10,   0,   0, -10,   0,   0
+
+};
+
+// rook positional score
+const int rook_score[64] =
+{
+	50,  50,  50,  50,  50,  50,  50,  50,
+	50,  50,  50,  50,  50,  50,  50,  50,
+	 0,   0,  10,  20,  20,  10,   0,   0,
+	 0,   0,  10,  20,  20,  10,   0,   0,
+	 0,   0,  10,  20,  20,  10,   0,   0,
+	 0,   0,  10,  20,  20,  10,   0,   0,
+	 0,   0,  10,  20,  20,  10,   0,   0,
+	 0,   0,   0,  20,  20,   0,   0,   0
+
+};
+
+// queen positional score
+const int queen_score[64] =
+{
+	 0,   0,   0,   0,   0,   0,   0,   0,
+	 0,   0,   0,   0,   0,   0,   0,   0,
+	 0,   0,   0,   0,   0,   0,   0,   0,
+	 0,   0,   0,   0,   0,   0,   0,   0,
+	 0,   0,   0,   0,   0,   0,   0,   0,
+	 0,   0,   0,   0,   0,   0,   0,   0,
+	 0,   0,   0,   0,   0,   0,   0,   0,
+	 0,   0,   0,   0,   0,   0,   0,   0
+};
+
+// king positional score
+const int king_score[64] =
+{
+	 0,   0,   0,   0,   0,   0,   0,   0,
+	 0,   0,   5,   5,   5,   5,   0,   0,
+	 0,   5,   5,  10,  10,   5,   5,   0,
+	 0,   5,  10,  20,  20,  10,   5,   0,
+	 0,   5,  10,  20,  20,  10,   5,   0,
+	 0,   0,   5,  10,  10,   5,   0,   0,
+	 0,   5,   5,  -5,  -5,   0,   5,   0,
+	 0,   0,   5,   0, -15,   0,  10,   0
+};
+
+// mirror positional score tables for opposite side
+const int mirror_score[64] =
+{
+	a1, b1, c1, d1, e1, f1, g1, h1,
+	a2, b2, c2, d2, e2, f2, g2, h2,
+	a3, b3, c3, d3, e3, f3, g3, h3,
+	a4, b4, c4, d4, e4, f4, g4, h4,
+	a5, b5, c5, d5, e5, f5, g5, h5,
+	a6, b6, c6, d6, e6, f6, g6, h6,
+	a7, b7, c7, d7, e7, f7, g7, h7,
+	a8, b8, c8, d8, e8, f8, g8, h8
+};
+
+int all_scores[6][64];
+
+// position evaluation
+static inline int evaluate()
+{
+	// evaluation score
+	int score = 0, square;
+
+	// current pieces bitboard copy
+	U64 bitboard;
+
+	//have to modify if you want to weight based on the position
+	// loop over piece bitboards
+	for (int bb_piece = P; bb_piece <= k; bb_piece++)
+	{
+		// init piece bitboard copy
+		bitboard = bitboards[bb_piece];
+
+		// loop over pieces within a bitboard
+		while (bitboard)
+		{
+			// init square
+			square = get_ls1b_index(bitboard);
+
+			// score material weights
+			score += material_score[bb_piece];
+
+			// score positional piece scores
+			score += (bb_piece <= 5) ? all_scores[bb_piece][square] : -all_scores[bb_piece - 6][mirror_score[square]];
+
+			// pop ls1b
+			pop_bit(bitboard, square);
+		}
+	}
+
+	// return final evaluation based on side
+	return (side == white) ? score : -score;
+}
+
+
+
+
 // Print BitBoard
+
 
 void print_bitboard(U64 bitboard) {
 	//loop ranks
@@ -1567,11 +1724,23 @@ void init_char() {
 	char_pieces['k'] = k;
 }
 
+void init_score() {
+	for (int i = 0; i < 64; i++) {
+		all_scores[0][i] = pawn_score[i];
+		all_scores[1][i] = knight_score[i];
+		all_scores[2][i] = bishop_score[i];
+		all_scores[3][i] = rook_score[i];
+		all_scores[4][i] = queen_score[i];
+		all_scores[5][i] = king_score[i];
+	}
+}
+
 void init_all() {
 	init_char();
 	init_leaper_attacks();
 	init_slider_attacks(bishop);
 	init_slider_attacks(rook);
+	init_score();
 	//init_bitboards(); LATER Add this for performance when you will need to implement multiple boards
 }
 
@@ -1579,21 +1748,10 @@ int main() {
 	U64 bitboard = 0ULL;
 	//init
 	init_all();
-	parse_FEN(start_position);
+	parse_FEN("rnbqkbnr/pppp1ppp/8/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 0 1 ");
 
 	print_board();
-	char input[8];
-	while (1) {
-		cin >> input;
 
-		if (input == "-1") break;
-		int move = parse_move(input);
-
-		if (move) {
-			make_move(move, all_moves);
-		}
-
-		print_board();
-	}
+	cout << "Score: " << evaluate() << endl;
 	return 0;
 }
