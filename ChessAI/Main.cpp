952969bb2c84d008239481415e8429ff84295d43
Maser-GitHub <simone000161@gmail.com>
2022-07-29 16:28:19 +0200

    Finish evaluation and start refactoring

--- ChessAI/Main.cpp
+++ ChessAI/Main.cpp
@@ -307,6 +307,31 @@ const int mirror_score[64] =
 
 int all_scores[6][64];
 
+//Pawn attacks table [side][square]
+
+U64 pawn_attacks[2][64];
+
+//Knight attacks table[square]
+
+U64 knight_attacks[64];
+
+//King attacks table[square]
+
+U64 king_attacks[64];
+
+//Bishop attack masks table[square]
+U64 bishop_masks[64];
+
+// Bishop attacks table[square][occupancies]
+U64 bishop_attacks[64][512];
+
+//Rook attacks mask[square]
+
+U64 rook_masks[64];
+
+//Rook attacks table[square][occupancies]
+U64 rook_attacks[64][4096];
+
 U64 set_file_rank_masks(int file, int rank) {
 	U64 mask = 0ULL;
 
@@ -322,11 +347,297 @@ U64 set_file_rank_masks(int file, int rank) {
 	return mask;
 }
 
+//Those magic numbers are needed in order to filter the rook attack given the current occupancy of the board
+
+// rook magic numbers
+U64 rook_magic_numbers[64] = {
+	0x8a80104000800020ULL,
+	0x140002000100040ULL,
+	0x2801880a0017001ULL,
+	0x100081001000420ULL,
+	0x200020010080420ULL,
+	0x3001c0002010008ULL,
+	0x8480008002000100ULL,
+	0x2080088004402900ULL,
+	0x800098204000ULL,
+	0x2024401000200040ULL,
+	0x100802000801000ULL,
+	0x120800800801000ULL,
+	0x208808088000400ULL,
+	0x2802200800400ULL,
+	0x2200800100020080ULL,
+	0x801000060821100ULL,
+	0x80044006422000ULL,
+	0x100808020004000ULL,
+	0x12108a0010204200ULL,
+	0x140848010000802ULL,
+	0x481828014002800ULL,
+	0x8094004002004100ULL,
+	0x4010040010010802ULL,
+	0x20008806104ULL,
+	0x100400080208000ULL,
+	0x2040002120081000ULL,
+	0x21200680100081ULL,
+	0x20100080080080ULL,
+	0x2000a00200410ULL,
+	0x20080800400ULL,
+	0x80088400100102ULL,
+	0x80004600042881ULL,
+	0x4040008040800020ULL,
+	0x440003000200801ULL,
+	0x4200011004500ULL,
+	0x188020010100100ULL,
+	0x14800401802800ULL,
+	0x2080040080800200ULL,
+	0x124080204001001ULL,
+	0x200046502000484ULL,
+	0x480400080088020ULL,
+	0x1000422010034000ULL,
+	0x30200100110040ULL,
+	0x100021010009ULL,
+	0x2002080100110004ULL,
+	0x202008004008002ULL,
+	0x20020004010100ULL,
+	0x2048440040820001ULL,
+	0x101002200408200ULL,
+	0x40802000401080ULL,
+	0x4008142004410100ULL,
+	0x2060820c0120200ULL,
+	0x1001004080100ULL,
+	0x20c020080040080ULL,
+	0x2935610830022400ULL,
+	0x44440041009200ULL,
+	0x280001040802101ULL,
+	0x2100190040002085ULL,
+	0x80c0084100102001ULL,
+	0x4024081001000421ULL,
+	0x20030a0244872ULL,
+	0x12001008414402ULL,
+	0x2006104900a0804ULL,
+	0x1004081002402ULL
+};
+
+// bishop magic numbers
+U64 bishop_magic_numbers[64] = {
+	0x40040844404084ULL,
+	0x2004208a004208ULL,
+	0x10190041080202ULL,
+	0x108060845042010ULL,
+	0x581104180800210ULL,
+	0x2112080446200010ULL,
+	0x1080820820060210ULL,
+	0x3c0808410220200ULL,
+	0x4050404440404ULL,
+	0x21001420088ULL,
+	0x24d0080801082102ULL,
+	0x1020a0a020400ULL,
+	0x40308200402ULL,
+	0x4011002100800ULL,
+	0x401484104104005ULL,
+	0x801010402020200ULL,
+	0x400210c3880100ULL,
+	0x404022024108200ULL,
+	0x810018200204102ULL,
+	0x4002801a02003ULL,
+	0x85040820080400ULL,
+	0x810102c808880400ULL,
+	0xe900410884800ULL,
+	0x8002020480840102ULL,
+	0x220200865090201ULL,
+	0x2010100a02021202ULL,
+	0x152048408022401ULL,
+	0x20080002081110ULL,
+	0x4001001021004000ULL,
+	0x800040400a011002ULL,
+	0xe4004081011002ULL,
+	0x1c004001012080ULL,
+	0x8004200962a00220ULL,
+	0x8422100208500202ULL,
+	0x2000402200300c08ULL,
+	0x8646020080080080ULL,
+	0x80020a0200100808ULL,
+	0x2010004880111000ULL,
+	0x623000a080011400ULL,
+	0x42008c0340209202ULL,
+	0x209188240001000ULL,
+	0x400408a884001800ULL,
+	0x110400a6080400ULL,
+	0x1840060a44020800ULL,
+	0x90080104000041ULL,
+	0x201011000808101ULL,
+	0x1a2208080504f080ULL,
+	0x8012020600211212ULL,
+	0x500861011240000ULL,
+	0x180806108200800ULL,
+	0x4000020e01040044ULL,
+	0x300000261044000aULL,
+	0x802241102020002ULL,
+	0x20906061210001ULL,
+	0x5a84841004010310ULL,
+	0x4010801011c04ULL,
+	0xa010109502200ULL,
+	0x4a02012000ULL,
+	0x500201010098b028ULL,
+	0x8040002811040900ULL,
+	0x28000010020204ULL,
+	0x6000020202d0240ULL,
+	0x8918844842082200ULL,
+	0x4010011029020020ULL
+};
+
+
+// ATTACKS
+
+// Mask all that is not a-file
+const U64 not_a_file = 0xfefefefefefefefeULL;
+// Mask all that is not h-file
+const U64 not_h_file = 0x7f7f7f7f7f7f7f7fULL;
+// Mask all that is not a-file or b-file
+const U64 not_ab_file = 0xfcfcfcfcfcfcfcfcULL;
+// Mask all that is not h-file or h-file
+const U64 not_hg_file = 0x3f3f3f3f3f3f3f3fULL;
+
+// number of squares attacked from bishop
+const int bishop_relevant_bits[64]{
+	 6, 5, 5, 5, 5, 5, 5, 6,
+	 5, 5, 5, 5, 5, 5, 5, 5,
+	 5, 5, 7, 7, 7, 7, 5, 5,
+	 5, 5, 7, 9, 9, 7, 5, 5,
+	 5, 5, 7, 9, 9, 7, 5, 5,
+	 5, 5, 7, 7, 7, 7, 5, 5,
+	 5, 5, 5, 5, 5, 5, 5, 5,
+	 6, 5, 5, 5, 5, 5, 5, 6
+};
+
+// number of squares attacked from rook
+const int rook_relevant_bits[64]{
+	12, 11, 11, 11, 11, 11, 11, 12,
+	11, 10, 10, 10, 10, 10, 10, 11,
+	11, 10, 10, 10, 10, 10, 10, 11,
+	11, 10, 10, 10, 10, 10, 10, 11,
+	11, 10, 10, 10, 10, 10, 10, 11,
+	11, 10, 10, 10, 10, 10, 10, 11,
+	11, 10, 10, 10, 10, 10, 10, 11,
+	12, 11, 11, 11, 11, 11, 11, 12
+};
+
+// castling rights update constants
+const int castling_rights[64] = {
+	 7, 15, 15, 15,  3, 15, 15, 11,
+	15, 15, 15, 15, 15, 15, 15, 15,
+	15, 15, 15, 15, 15, 15, 15, 15,
+	15, 15, 15, 15, 15, 15, 15, 15,
+	15, 15, 15, 15, 15, 15, 15, 15,
+	15, 15, 15, 15, 15, 15, 15, 15,
+	15, 15, 15, 15, 15, 15, 15, 15,
+	13, 15, 15, 15, 12, 15, 15, 14
+};
+
+
+//get bishop attacks
+static inline U64 get_bishop_attacks(int square, U64 occupancy) {
+	// getting bishop attack given the current occupancy
+	occupancy &= bishop_masks[square];
+	occupancy *= bishop_magic_numbers[square];
+	occupancy >>= 64 - (U64)bishop_relevant_bits[square];
+
+	return bishop_attacks[square][occupancy];
+}
+
+//get rook attacks
+static inline U64 get_rook_attacks(int square, U64 occupancy) {
+	// getting bishop attack given the current occupancy
+	occupancy &= rook_masks[square];
+	occupancy *= rook_magic_numbers[square];
+	occupancy >>= 64 - rook_relevant_bits[square];
+
+	return rook_attacks[square][occupancy];
+}
+
+//get queen attacks
+static inline U64 get_queen_attacks(int square, U64 occupancy) {
+	U64 b_occupancy = occupancy;
+	U64 r_occupancy = occupancy;
+
+	// getting bishop attack given the current occupancy
+	b_occupancy &= bishop_masks[square];
+	b_occupancy *= bishop_magic_numbers[square];
+	b_occupancy >>= 64 - bishop_relevant_bits[square];
+
+	// getting rook attack given the current occupancy
+	r_occupancy &= rook_masks[square];
+	r_occupancy *= rook_magic_numbers[square];
+	r_occupancy >>= 64 - rook_relevant_bits[square];
+
+	return bishop_attacks[square][b_occupancy] | rook_attacks[square][r_occupancy];
+}
+
+
+// MVV LVA [attacker][victim]
+static int mvv_lva[12][12] = {
+	105, 205, 305, 405, 505, 605,  105, 205, 305, 405, 505, 605,
+	104, 204, 304, 404, 504, 604,  104, 204, 304, 404, 504, 604,
+	103, 203, 303, 403, 503, 603,  103, 203, 303, 403, 503, 603,
+	102, 202, 302, 402, 502, 602,  102, 202, 302, 402, 502, 602,
+	101, 201, 301, 401, 501, 601,  101, 201, 301, 401, 501, 601,
+	100, 200, 300, 400, 500, 600,  100, 200, 300, 400, 500, 600,
+
+	105, 205, 305, 405, 505, 605,  105, 205, 305, 405, 505, 605,
+	104, 204, 304, 404, 504, 604,  104, 204, 304, 404, 504, 604,
+	103, 203, 303, 403, 503, 603,  103, 203, 303, 403, 503, 603,
+	102, 202, 302, 402, 502, 602,  102, 202, 302, 402, 502, 602,
+	101, 201, 301, 401, 501, 601,  101, 201, 301, 401, 501, 601,
+	100, 200, 300, 400, 500, 600,  100, 200, 300, 400, 500, 600
+};
+
+// killer moves [id][ply]
+int killer_moves[2][PLY];
+
+// history moves [piece][square]
+int history_moves[12][64];
+
+//file masks
+U64 file_masks[64];
+//rank masks
+U64 rank_masks[64];
+
+U64 isolated_masks[64];
+
+U64 white_passed_masks[64];
+
+U64 black_passed_masks[64];
+
+const int get_rank[64] =
+{
+	7, 7, 7, 7, 7, 7, 7, 7,
+	6, 6, 6, 6, 6, 6, 6, 6,
+	5, 5, 5, 5, 5, 5, 5, 5,
+	4, 4, 4, 4, 4, 4, 4, 4,
+	3, 3, 3, 3, 3, 3, 3, 3,
+	2, 2, 2, 2, 2, 2, 2, 2,
+	1, 1, 1, 1, 1, 1, 1, 1,
+	0, 0, 0, 0, 0, 0, 0, 0
+};
+
+const int double_pawn_penalty = -10;
+
+const int isolated_pawn_penalty = -10;
+
+const int passed_pawn_bonus[8] = { 0, 10, 30, 50, 75, 100, 150, 200 };
+
+const int semi_open_file_score = 10;
+
+const int open_file_score = 15;
+
+const int king_shield_bonus = 5;
+
 // position evaluation
 static inline int evaluate()
 {
 	// evaluation score
 	int score = 0, square;
+	// penalty
+	int double_pawns = 0;
 
 	// current pieces bitboard copy
 	U64 bitboard;
@@ -347,9 +658,104 @@ static inline int evaluate()
 			// score material weights
 			score += material_score[bb_piece];
 
-			// score positional piece scores
 			score += (bb_piece <= 5) ? all_scores[bb_piece][square] : -all_scores[bb_piece - 6][mirror_score[square]];
 
+			//Extra evaluation with pawns penalty and open/semiopen files
+			switch (bb_piece) {
+			case P:
+				// double pawn penalty
+				double_pawns = count_bits(bitboards[P] & file_masks[square]);
+
+				// double pawns
+				if (double_pawns > 1)
+					score += double_pawns * double_pawn_penalty;
+
+				// isolated pawn
+				if ((bitboards[P] & isolated_masks[square]) == 0)
+					score += isolated_pawn_penalty;
+
+				// passed pawn
+				if ((white_passed_masks[square] & bitboards[p]) == 0)
+					score += passed_pawn_bonus[get_rank[square]];
+				break;
+			case p:
+				// double pawn penalty
+				double_pawns = count_bits(bitboards[p] & file_masks[square]);
+
+				// double pawns
+				if (double_pawns > 1)
+					score -= double_pawns * double_pawn_penalty;
+
+				// isolated pawnd
+				if ((bitboards[p] & isolated_masks[square]) == 0)
+					score -= isolated_pawn_penalty;
+
+				// passed pawn
+				if ((black_passed_masks[square] & bitboards[P]) == 0)
+					score -= passed_pawn_bonus[get_rank[mirror_score[square]]];
+				break;
+			case R:
+				// semi open file
+				if ((bitboards[P] & file_masks[square]) == 0)
+					score += semi_open_file_score;
+
+				// semi open file
+				if (((bitboards[P] | bitboards[p]) & file_masks[square]) == 0)
+					score += open_file_score;
+				break;
+
+
+			case r:
+				// semi open file
+				if ((bitboards[p] & file_masks[square]) == 0)
+					score -= semi_open_file_score;
+
+				// semi open file
+				if (((bitboards[P] | bitboards[p]) & file_masks[square]) == 0)
+					score -= open_file_score;
+
+				break;
+			case K:
+				// semi open file
+				if ((bitboards[P] & file_masks[square]) == 0)
+					score -= semi_open_file_score;
+
+				// semi open file
+				if (((bitboards[P] | bitboards[p]) & file_masks[square]) == 0)
+					score -= open_file_score;
+
+				// king safety bonus
+				score += count_bits(king_attacks[square] & occupancies[white]) * king_shield_bonus;
+				break;
+			case k:
+				// semi open file
+				if ((bitboards[p] & file_masks[square]) == 0)
+					score += semi_open_file_score;
+
+				// semi open file
+				if (((bitboards[P] | bitboards[p]) & file_masks[square]) == 0)
+					score += open_file_score;
+
+				// king safety bonus
+				score -= count_bits(king_attacks[square] & occupancies[black]) * king_shield_bonus;
+				break;
+			case B:
+				// mobility
+				score += count_bits(get_bishop_attacks(square, occupancies[both]));
+				break;
+			case b:
+				// mobility
+				score -= count_bits(get_bishop_attacks(square, occupancies[both]));
+				break;
+			case Q:
+				// mobility
+				score += count_bits(get_queen_attacks(square, occupancies[both]));
+				break;
+			case q:
+				// mobility
+				score -= count_bits(get_queen_attacks(square, occupancies[both]));
+				break;
+			}
 			// pop ls1b
 			pop_bit(bitboard, square);
 		}
@@ -582,66 +988,6 @@ void parse_FEN(const char* fen) {
 }
 
 
-// ATTACKS
-
-// Mask all that is not a-file
-const U64 not_a_file = 0xfefefefefefefefeULL;
-// Mask all that is not h-file
-const U64 not_h_file = 0x7f7f7f7f7f7f7f7fULL;
-// Mask all that is not a-file or b-file
-const U64 not_ab_file = 0xfcfcfcfcfcfcfcfcULL;
-// Mask all that is not h-file or h-file
-const U64 not_hg_file = 0x3f3f3f3f3f3f3f3fULL;
-
-//file masks
-U64 file_masks[64];
-//rank masks
-U64 rank_masks[64];
-
-U64 isolated_masks[64];
-
-U64 white_passed_masks[64];
-
-U64 black_passed_masks[64];
-
-
-
-// number of squares attacked from bishop
-const int bishop_relevant_bits[64]{
-	 6, 5, 5, 5, 5, 5, 5, 6,
-	 5, 5, 5, 5, 5, 5, 5, 5,
-	 5, 5, 7, 7, 7, 7, 5, 5,
-	 5, 5, 7, 9, 9, 7, 5, 5,
-	 5, 5, 7, 9, 9, 7, 5, 5,
-	 5, 5, 7, 7, 7, 7, 5, 5,
-	 5, 5, 5, 5, 5, 5, 5, 5,
-	 6, 5, 5, 5, 5, 5, 5, 6
-};
-
-// number of squares attacked from rook
-const int rook_relevant_bits[64]{
-	12, 11, 11, 11, 11, 11, 11, 12,
-	11, 10, 10, 10, 10, 10, 10, 11,
-	11, 10, 10, 10, 10, 10, 10, 11,
-	11, 10, 10, 10, 10, 10, 10, 11,
-	11, 10, 10, 10, 10, 10, 10, 11,
-	11, 10, 10, 10, 10, 10, 10, 11,
-	11, 10, 10, 10, 10, 10, 10, 11,
-	12, 11, 11, 11, 11, 11, 11, 12
-};
-
-// castling rights update constants
-const int castling_rights[64] = {
-	 7, 15, 15, 15,  3, 15, 15, 11,
-	15, 15, 15, 15, 15, 15, 15, 15,
-	15, 15, 15, 15, 15, 15, 15, 15,
-	15, 15, 15, 15, 15, 15, 15, 15,
-	15, 15, 15, 15, 15, 15, 15, 15,
-	15, 15, 15, 15, 15, 15, 15, 15,
-	15, 15, 15, 15, 15, 15, 15, 15,
-	13, 15, 15, 15, 12, 15, 15, 14
-};
-
 // pseudo random number state
 unsigned int random_state = 1804289383;
 
@@ -678,195 +1024,6 @@ U64 get_random_U64_number()
 	// return random number
 	return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
 }
-
-
-
-//Those magic numbers are needed in order to filter the rook attack given the current occupancy of the board
-
-// rook magic numbers
-U64 rook_magic_numbers[64] = {
-	0x8a80104000800020ULL,
-	0x140002000100040ULL,
-	0x2801880a0017001ULL,
-	0x100081001000420ULL,
-	0x200020010080420ULL,
-	0x3001c0002010008ULL,
-	0x8480008002000100ULL,
-	0x2080088004402900ULL,
-	0x800098204000ULL,
-	0x2024401000200040ULL,
-	0x100802000801000ULL,
-	0x120800800801000ULL,
-	0x208808088000400ULL,
-	0x2802200800400ULL,
-	0x2200800100020080ULL,
-	0x801000060821100ULL,
-	0x80044006422000ULL,
-	0x100808020004000ULL,
-	0x12108a0010204200ULL,
-	0x140848010000802ULL,
-	0x481828014002800ULL,
-	0x8094004002004100ULL,
-	0x4010040010010802ULL,
-	0x20008806104ULL,
-	0x100400080208000ULL,
-	0x2040002120081000ULL,
-	0x21200680100081ULL,
-	0x20100080080080ULL,
-	0x2000a00200410ULL,
-	0x20080800400ULL,
-	0x80088400100102ULL,
-	0x80004600042881ULL,
-	0x4040008040800020ULL,
-	0x440003000200801ULL,
-	0x4200011004500ULL,
-	0x188020010100100ULL,
-	0x14800401802800ULL,
-	0x2080040080800200ULL,
-	0x124080204001001ULL,
-	0x200046502000484ULL,
-	0x480400080088020ULL,
-	0x1000422010034000ULL,
-	0x30200100110040ULL,
-	0x100021010009ULL,
-	0x2002080100110004ULL,
-	0x202008004008002ULL,
-	0x20020004010100ULL,
-	0x2048440040820001ULL,
-	0x101002200408200ULL,
-	0x40802000401080ULL,
-	0x4008142004410100ULL,
-	0x2060820c0120200ULL,
-	0x1001004080100ULL,
-	0x20c020080040080ULL,
-	0x2935610830022400ULL,
-	0x44440041009200ULL,
-	0x280001040802101ULL,
-	0x2100190040002085ULL,
-	0x80c0084100102001ULL,
-	0x4024081001000421ULL,
-	0x20030a0244872ULL,
-	0x12001008414402ULL,
-	0x2006104900a0804ULL,
-	0x1004081002402ULL
-};
-
-// bishop magic numbers
-U64 bishop_magic_numbers[64] = {
-	0x40040844404084ULL,
-	0x2004208a004208ULL,
-	0x10190041080202ULL,
-	0x108060845042010ULL,
-	0x581104180800210ULL,
-	0x2112080446200010ULL,
-	0x1080820820060210ULL,
-	0x3c0808410220200ULL,
-	0x4050404440404ULL,
-	0x21001420088ULL,
-	0x24d0080801082102ULL,
-	0x1020a0a020400ULL,
-	0x40308200402ULL,
-	0x4011002100800ULL,
-	0x401484104104005ULL,
-	0x801010402020200ULL,
-	0x400210c3880100ULL,
-	0x404022024108200ULL,
-	0x810018200204102ULL,
-	0x4002801a02003ULL,
-	0x85040820080400ULL,
-	0x810102c808880400ULL,
-	0xe900410884800ULL,
-	0x8002020480840102ULL,
-	0x220200865090201ULL,
-	0x2010100a02021202ULL,
-	0x152048408022401ULL,
-	0x20080002081110ULL,
-	0x4001001021004000ULL,
-	0x800040400a011002ULL,
-	0xe4004081011002ULL,
-	0x1c004001012080ULL,
-	0x8004200962a00220ULL,
-	0x8422100208500202ULL,
-	0x2000402200300c08ULL,
-	0x8646020080080080ULL,
-	0x80020a0200100808ULL,
-	0x2010004880111000ULL,
-	0x623000a080011400ULL,
-	0x42008c0340209202ULL,
-	0x209188240001000ULL,
-	0x400408a884001800ULL,
-	0x110400a6080400ULL,
-	0x1840060a44020800ULL,
-	0x90080104000041ULL,
-	0x201011000808101ULL,
-	0x1a2208080504f080ULL,
-	0x8012020600211212ULL,
-	0x500861011240000ULL,
-	0x180806108200800ULL,
-	0x4000020e01040044ULL,
-	0x300000261044000aULL,
-	0x802241102020002ULL,
-	0x20906061210001ULL,
-	0x5a84841004010310ULL,
-	0x4010801011c04ULL,
-	0xa010109502200ULL,
-	0x4a02012000ULL,
-	0x500201010098b028ULL,
-	0x8040002811040900ULL,
-	0x28000010020204ULL,
-	0x6000020202d0240ULL,
-	0x8918844842082200ULL,
-	0x4010011029020020ULL
-};
-
-// MVV LVA [attacker][victim]
-static int mvv_lva[12][12] = {
-	105, 205, 305, 405, 505, 605,  105, 205, 305, 405, 505, 605,
-	104, 204, 304, 404, 504, 604,  104, 204, 304, 404, 504, 604,
-	103, 203, 303, 403, 503, 603,  103, 203, 303, 403, 503, 603,
-	102, 202, 302, 402, 502, 602,  102, 202, 302, 402, 502, 602,
-	101, 201, 301, 401, 501, 601,  101, 201, 301, 401, 501, 601,
-	100, 200, 300, 400, 500, 600,  100, 200, 300, 400, 500, 600,
-
-	105, 205, 305, 405, 505, 605,  105, 205, 305, 405, 505, 605,
-	104, 204, 304, 404, 504, 604,  104, 204, 304, 404, 504, 604,
-	103, 203, 303, 403, 503, 603,  103, 203, 303, 403, 503, 603,
-	102, 202, 302, 402, 502, 602,  102, 202, 302, 402, 502, 602,
-	101, 201, 301, 401, 501, 601,  101, 201, 301, 401, 501, 601,
-	100, 200, 300, 400, 500, 600,  100, 200, 300, 400, 500, 600
-};
-
-// killer moves [id][ply]
-int killer_moves[2][PLY];
-
-// history moves [piece][square]
-int history_moves[12][64];
-
-
-//Pawn attacks table [side][square]
-
-U64 pawn_attacks[2][64];
-
-//Knight attacks table[square]
-
-U64 knight_attacks[64];
-
-//King attacks table[square]
-
-U64 king_attacks[64];
-
-//Bishop attack masks table[square]
-U64 bishop_masks[64];
-
-// Bishop attacks table[square][occupancies]
-U64 bishop_attacks[64][512];
-
-//Rook attacks mask[square]
-
-U64 rook_masks[64];
-
-//Rook attacks table[square][occupancies]
-U64 rook_attacks[64][4096];
 
 //generate mask pawn attack
 U64 mask_pawn_attacks(int side, int square) {
@@ -1103,43 +1260,6 @@ void init_slider_attacks(int bishop) {
 	}
 }
 
-//get bishop attacks
-static inline U64 get_bishop_attacks(int square, U64 occupancy) {
-	// getting bishop attack given the current occupancy
-	occupancy &= bishop_masks[square];
-	occupancy *= bishop_magic_numbers[square];
-	occupancy >>= 64 - bishop_relevant_bits[square];
-
-	return bishop_attacks[square][occupancy];
-}
-
-//get rook attacks
-static inline U64 get_rook_attacks(int square, U64 occupancy) {
-	// getting bishop attack given the current occupancy
-	occupancy &= rook_masks[square];
-	occupancy *= rook_magic_numbers[square];
-	occupancy >>= 64 - rook_relevant_bits[square];
-
-	return rook_attacks[square][occupancy];
-}
-
-//get queen attacks
-static inline U64 get_queen_attacks(int square, U64 occupancy) {
-	U64 b_occupancy = occupancy;
-	U64 r_occupancy = occupancy;
-
-	// getting bishop attack given the current occupancy
-	b_occupancy &= bishop_masks[square];
-	b_occupancy *= bishop_magic_numbers[square];
-	b_occupancy >>= 64 - bishop_relevant_bits[square];
-
-	// getting rook attack given the current occupancy
-	r_occupancy &= rook_masks[square];
-	r_occupancy *= rook_magic_numbers[square];
-	r_occupancy >>= 64 - rook_relevant_bits[square];
-
-	return bishop_attacks[square][b_occupancy] | rook_attacks[square][r_occupancy];
-}
 
 // TODO could be optimized
 //Checks if square is attacked
@@ -1880,7 +2000,7 @@ static inline int read_hash_entry(int alpha, int beta, int depth) {
 	int hash_score = get<3>(hash_entry);
 	if (get<0>(hash_entry) == hash_key) {
 		if (get<1>(hash_entry) >= depth) {
-			
+
 			if (hash_score < -MATE_SCORE) hash_score += ply;
 			if (hash_score > MATE_SCORE) hash_score -= ply;
 
